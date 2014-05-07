a4608daebe5906e753faa89e8059219a458020b9
JP Akibako <jp@akibako>
2014-05-07 16:04:35 -0600

    SSH tweak.

--- modules/puppet/files/pull-updates.sh
+++ modules/puppet/files/pull-updates.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+eval `keychain --agents ssh --eval ~/.ssh/puppet-deploy`
 git pull git@github.com:blunxy/mypuppet.git
 cd mypuppet
-mv -r * /etc/puppet/
+suod mv -r * /etc/puppet/
 cd /etc/puppet
 /usr/local/bin/papply
